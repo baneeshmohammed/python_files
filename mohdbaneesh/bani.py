d3a2922edade7c50b27d3c9ceaf291c6677d8721
baneeshmohammed <139439665+baneeshmohammed@users.noreply.github.com>
2023-10-11 13:30:52 +0530

    python conditional statements

---
+++ mohdbaneesh/bani.py
@@ -0,0 +1,56 @@
+
+# print(s)
+# x=["apple",1,2,3,3,]
+# y=list(x)
+# y[2]="grape"
+# y.append("orange")
+# y.pop(1)
+# print(y)
+# name="mohammed baneesh"
+# x=name.upper()
+# print(x)
+
+
+# if 5>2:
+#     print("true")
+# else:
+#     print("false")
+
+# i=int(input("Enter:"))
+# if i>10:
+#     print("greater")
+# elif i<10:
+#     print("not greater")
+# else:
+#     print("may be equal")   
+
+
+# a=10
+# b=15
+# c=8
+# if a>b:
+#     if a>c:
+#         print(a,"is greater")
+#     else:
+#         print(b,"is greater")  
+
+# i=int(input("Enter:"))
+# if i%7==0:
+#     print("yes")
+# else:
+#     print("no")
+
+
+
+# n=int(input("Enter:"))
+# n=n%10
+# print(n)
+
+
+# i=int(input("enter:"))
+# n=i%10
+# if n%3==0:
+#     print("divisible")
+# else:
+#      print("not divisble")    
+
